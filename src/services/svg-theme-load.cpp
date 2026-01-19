72fc4ab46fb5bf66b9ba91c4fd664c9b56aaca74
Paul Dempsey <pcdempsey@live.com>
2026-01-19 10:01:41 -0800

    Fix Linux strncpy build warning

--- src/services/svg-theme-load.cpp
+++ src/services/svg-theme-load.cpp
@@ -746,7 +746,7 @@ std::shared_ptr<SvgTheme> loadSvgThemeFile(std::string path, ErrorContext* error
             if (!name.empty()) {
                 theme->name = name;
                 if (error_context) {
-                    strncpy(error_context->theme_name, name.c_str(), sizeof(error_context->theme_name));
+                    strncpy(error_context->theme_name, name.c_str(), sizeof(error_context->theme_name)-1);
                 }
             }
         } else {
